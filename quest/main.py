ca4051b2486660a3f703cfe363068cd1c305cefb
Anand Chitipothu <anandology@gmail.com>
2014-12-16 22:50:37 +0530

    fixed error in starting heroku app.

--- quest/main.py
+++ quest/main.py
@@ -1,10 +1,22 @@
 import web
-from . import config
+import logging
 
+def setup_logging():
+    FORMAT = "%(asctime)-15s [%(levelname)s] %(message)s"
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+
+app = web.auto_application()
+application = app.wsgifunc()
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+from . import config
+logger.info("loading config")
 config.load_config()
 
 from webapp import app
-
 application = app.wsgifunc()
 
 # Heroku doesn't handle static files, use StaticMiddleware.
