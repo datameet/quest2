6d784611ab25226eba1ead64912ce514072c254a
Anand Chitipothu <anandology@gmail.com>
2014-12-16 23:06:32 +0530

    Serve schema files.

--- quest/webapp.py
+++ quest/webapp.py
@@ -1,11 +1,14 @@
 import web
 from .db import Dataset
 from .template import render_template
+import os
 
 urls = (
-    "/", "index"
+    "/", "index",
+    "/schema/(.*)", "schema",
 )
 app = web.application(urls, globals())
+
 
 class index:
     def GET(self):
@@ -13,3 +16,27 @@ class index:
         return render_template("index.html", datasets=datasets)
 
 
+class schema:
+    """We want to serve the schema files under /schema/.
+    """
+    def GET(self, filename):
+        if ".." in filename:
+            raise web.notfound()
+
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, "static", "schema", filename)
+        print "path", path
+
+        if not os.path.exists(path):
+            raise web.notfound()
+
+        web.header("Content-type", self.get_content_type(filename))
+        return open(path).read()
+
+    def get_content_type(self, filename):
+        if filename.endswith(".jsonld"):
+            return "application/ld+json"
+        elif filename.endswith(".json"):
+            return "application/json"
+        else:
+            return "application/octet-stream"
